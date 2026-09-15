713f3af84bff353194ff76f08530640a4882912e
Tracy Magz <gamerthann@gmail.com>
2026-09-15 22:06:03 +0700

    comment google service

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:woc/provider/user_provider.dart';
 import 'package:woc/main_wrapper.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+//import 'package:google_sign_in/google_sign_in.dart';
 
 void main() async {
-
+/*
   // สั่งให้ระบบ Flutter Engine เชื่อมต่อและสื่อสารกับตัวระบบปฏิบัติการให้เสร็จสมบูรณ์ก่อนเริ่มการทำงาน
   WidgetsFlutterBinding.ensureInitialized();
   
@@ -14,7 +14,7 @@ void main() async {
     // เป็นเสมือนตราประทับเพื่อตรวจสอบว่า id ที่ไปเอามาจาก google คืออันเดียวกับที่ผูกไว้กับส่วนของ backend
     serverClientId: "322948964487-s1653nkp0eq497iq4jrq2du61p9af8o1.apps.googleusercontent.com",
   );
-
+*/
   runApp(ChangeNotifierProvider(create: (_) => UserProvider(), child: const MyApp()));
 }
 
